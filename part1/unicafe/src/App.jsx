cdcc5afa5b365e99f12a7d429fc804a05c48ea8c
Francisco <franciscoosmarflores@gmail.com>
2025-03-05 09:11:06 -0300

    {ADD} solving exercise 1.5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,52 +1,69 @@
+import { useState } from 'react'
+
+const Header = ({title}) =>{
+  return(
+    <h1>{title}</h1>
+  )
+}
+
+const Button = ({handleclick, text}) =>{
+  return (
+    <button onClick={handleclick}>{text}</button>
+  )
+}
+const Statistics = ({ feedback }) => {
+  const total = Object.values(feedback).reduce((sum, value) => sum + value, 0);
+
+  const lines = Object.entries(feedback).map(([text, value]) => (
+    <StatisticLine key={text} text={text} value={value} />
+  ));
+
+  return total === 0 ? (
+    <a>We don't have feedbacks for statistics</a>
+  ) : (
+      <table>{lines}</table>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+    <td>{text}</td><td>{value}</td>
+    </tr>
+  ) 
+}
+
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {name: 'Fundamentals of React', exercises: 10},
-    {name: 'Using props to pass data', exercises: 7},
-    {name: 'State of a component', exercises: 14},
-  ]
-  
+  // guarda los clics de cada botón en su propio estado
+
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
+
+  const handleGoodClick= ()=>{
+    console.log("good ", feedback)
+    setFeedback({...feedback, good: feedback.good+1})
+  }
+  const handleBadClick= ()=>{
+    console.log("bad ", feedback)
+    setFeedback({...feedback, bad: feedback.bad+1})
+  }
+  const handleNeutralClick= ()=>{
+    console.log("neutral ", feedback)
+    setFeedback({...feedback, neutral: feedback.neutral+1})
+  }
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header title={'Give feedback!'}/>
+
+      <Button handleclick={handleGoodClick} text={'good'}/>
+      <Button handleclick={handleNeutralClick} text={'neutral'}/>
+      <Button handleclick={handleBadClick} text={'bad'}/>
+
+      <Header title={'Statistics'}/>
     </div>
-  )
-}
-
-const Header = ({course})=>{
-  return (
-    <h1>{course}</h1>
-  )
-}
-
-const Content = ({parts}) =>{
-  console.log("Content : ")
-
-  return (
-    <>
-      {parts.map(
-        (part, index) =>{
-          return <Part key={index} name={part.name} exercises={part.exercises} />
-        }
-      )}
-    </>
     
   )
 }
-
-const Part = ({ name, exercises})=>{
-  return (
-    <p>{name} {exercises}</p>
-  )
-}
-const Total = ({parts}) =>{
-  const total = parts.reduce((sum, part)=>sum+part.exercises,0)
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
-
 
 export default App
